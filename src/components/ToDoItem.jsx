999269f237f8572bfbd8cf40a841714291e9c46d
hiro <hmaeyama0510@gmail.com>
2021-05-21 20:17:13 +0900

    Add deleteItem function ・add key and id to detect the specific item ・filter function to return the items which index is not matching with id

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
-
-// const [line, setLine] = useState("");
+import React from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function handleClick() {
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
-
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
-        {props.text}
-      </li>
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
